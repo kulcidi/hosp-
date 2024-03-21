bc6127ce0aa95a70e5d68dac7572efd92d5cacf2
kulcidi <144631070+kulcidi@users.noreply.github.com>
2024-03-22 03:12:49 +0900

    Add files via upload

--- down1.js
+++ down1.js
@@ -1,30 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const targetElement = document.querySelector('.main__doctors');
+document.addEventListener("DOMContentLoaded", function () {
+  const targetElement = document.querySelector(".main__doctors");
 
-    const n1Element = document.getElementById('n1');
+  const n1Element = document.getElementById("n1");
 
-    n1Element.addEventListener('click', function() {
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-    });
+  n1Element.addEventListener("click", function () {
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const targetElement = document.querySelector('.main__map');
+document.addEventListener("DOMContentLoaded", function () {
+  const targetElement = document.querySelector("#button2");
 
-    const n1Element = document.getElementById('n2');
+  const n1Element = document.querySelector("#n2");
 
-    n1Element.addEventListener('click', function() {
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-    });
+  n1Element.addEventListener("click", function () {
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const targetElement = document.querySelector('.footer__needs');
+document.addEventListener("DOMContentLoaded", function () {
+  const targetElement = document.querySelector(".footer__needs");
 
-    const n1Element = document.getElementById('n3');
+  const n1Element = document.getElementById("n3");
 
-    n1Element.addEventListener('click', function() {
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-    });
+  n1Element.addEventListener("click", function () {
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  });
 });
-
